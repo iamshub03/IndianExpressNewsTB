318ddc2435c0cde46195b2e4902abd3cdf7c078a
Shubham Pandey <spandey@aisplstore.com>
2021-07-02 16:01:07 +0530

    Added news API

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const axios = require('axios');
 const bot = new Composer()
 
 bot.start((ctx) => {
-    ctx.reply(`Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian News Bot. This bot is in its early build stages but will soon have all the features required. The commands that you can use right now are: 1. /HeadLines_India`)
+    ctx.reply(`Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian News Bot. This bot is in its early build stages but will soon have all the features required. The commands that you can use right now are: 1. /HeadLinesIndia`)
 })
 
-bot.command('HeadLines_India', (ctx) => {
+bot.command('HeadLinesIndia', (ctx) => {
     let resultData = "";
     axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=11ed06385db04616a8bb78a13c2708d6")
     .then(d => {
