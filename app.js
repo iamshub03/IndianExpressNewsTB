c49e0b893121543ae0caa1c203957703e8304993
Shubham Pandey <spandey@aisplstore.com>
2021-07-02 14:50:50 +0530

    initial commit

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ bot.start((ctx) => {
     ctx.reply(`Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian Express News Bot. This bot is in its early build stages but will soon have all the features required. The commands that you can use right now are: 1. /news`)
 })
 
-bot.news((ctx) => {
-    ctx.reply("News for you!!!");
-})
-
 module.exports = bot
 
 // indian-express-news-bot
