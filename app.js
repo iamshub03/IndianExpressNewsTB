5c70b32c3e81301a32dd14cab8ddb0af4217c5e8
Shubham Pandey <spandey@aisplstore.com>
2021-07-02 13:38:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+        bot.telegram.sendMessage(ctx.chat.id, `Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian Express News Bot. This bot is in its early build stages but will soon have all the features required. The commands that you can use right now are: 1. /news`, {
+        })
+        .catch(err => console.log(err));
+})
+
+bot.launch();
