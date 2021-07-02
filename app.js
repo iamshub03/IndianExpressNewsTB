e6f167b5f51602fc94a01c4e3da5c22b3b4ac85c
Shubham Pandey <spandey@aisplstore.com>
2021-07-02 16:49:24 +0530

    Added news API

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const axios = require('axios');
 const bot = new Composer()
 
 bot.start((ctx) => {
-    ctx.reply(`Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian News Bot. This bot is in its early build stages but will soon have all the features required. The commands that you can use right now are: 1. /headlines`)
+    ctx.reply(`Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian News Bot. This bot is in its early build stages but will soon have all the features required.`)
 })
 
+
+//Headlines General
 bot.command('headlines', (ctx) => {
     let resultData = "";
     axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=11ed06385db04616a8bb78a13c2708d6")
@@ -23,6 +25,22 @@ bot.command('headlines', (ctx) => {
       });
     ctx.reply('News Headlines for India are ->');
 })
+
+//Sports Headlines
+bot.command('sports', (ctx) => {
+    axios.get("https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=11ed06385db04616a8bb78a13c2708d6")
+    .then(d => {
+        let headlines = d.data.articles;
+        headlines.forEach( headline => {
+            ctx.reply("=> " + headline.title);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+      });
+    ctx.reply('Sports News Headlines are ->');
+})
+
 
 module.exports = bot
 
