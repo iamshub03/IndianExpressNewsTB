e91703e7de2c1f112f99ae51b3dd8e62f540c90a
Shubham Pandey <spandey@aisplstore.com>
2021-07-02 14:47:39 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const BOT_TOKEN = `1736249180:AAGKeMsDJlBqUWklOM5fsEog8isJIM_wUrU`;
 
 const bot = new Composer()
 
-bot.command('start', ctx => {
-    console.log(ctx.from)
-        bot.telegram.sendMessage(ctx.chat.id, `Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian Express News Bot. This bot is in its early build stages but will soon have all the features required. The commands that you can use right now are: 1. /news`, {
-        })
-        .catch(err => console.log(err));
+bot.start((ctx) => {
+    ctx.reply(`Hey ${ctx.from.first_name}! My name is Shubham and welcome to my bot - The Indian Express News Bot. This bot is in its early build stages but will soon have all the features required. The commands that you can use right now are: 1. /news`)
 })
 
 module.exports = bot
